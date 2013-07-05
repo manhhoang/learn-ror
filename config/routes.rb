d5fba437bd50f9da44cf88dc68f2c53fb1707b43
Ha Manh Hoang <hamanhhoang1102@gmail.com>
2013-07-05 16:25:38 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LearnRails::Application.routes.draw do
+  get "welcome/index"
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
   root :to => 'visitors#new'
